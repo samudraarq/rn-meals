da35ae6f2e1abeb85c5e47ee9ba836fa869ccff5
Samudra Faria <samudrafaris@gmail.com>
2021-06-06 15:21:50 +0700

    favorite header button

--- navigation/MealsNavigator.js
+++ navigation/MealsNavigator.js
@@ -2,6 +2,7 @@ import { Platform } from "react-native";
 
 import { createAppContainer } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Colors from "../constants/Colors";
 
 import CategoriesScreen from "../screens/CategoriesScreen";
--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { Button, StyleSheet, Text, View } from "react-native";
+import { HeaderButtons, Item } from "react-navigation-header-buttons";
 import { MEALS } from "../data/dummy-data";
+import HeaderButton from "../components/HeaderButton";
 
 const MealDetailScreen = (props) => {
   const mealId = props.navigation.getParam("mealId");
@@ -25,6 +27,17 @@ MealDetailScreen.navigationOptions = (navigationData) => {
   const selectedMeal = MEALS.find((meal) => meal.id === mealId);
   return {
     headerTitle: selectedMeal.title,
+    headerRight: () => (
+      <HeaderButtons HeaderButtonComponent={HeaderButton}>
+        <Item
+          title="Favorite"
+          iconName="ios-star"
+          onPress={() => {
+            console.log("Mark as favorite");
+          }}
+        />
+      </HeaderButtons>
+    ),
   };
 };
 
